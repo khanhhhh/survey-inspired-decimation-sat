66bdd5a258ff310d39ad1add156cdf018ed4b62a
Nguyen Ngoc Khanh <nguyenngockhanh.pbc@gmail.com>
2020-01-04 22:32:14 +0800

    modified:   main.go 	renamed:    src/sid-sat/decimationImpl.go -> src/sat/decimationImpl.go 	renamed:    src/sid-sat/instance.go -> src/sat/instance.go 	renamed:    src/sid-sat/instanceImpl.go -> src/sat/instanceImpl.go 	renamed:    src/sid-sat/primitiveImpl.go -> src/sat/primitiveImpl.go 	renamed:    src/sid-sat/propagationImpl.go -> src/sat/propagationImpl.go 	renamed:    src/sid-sat/surveyDecimationImpl.go -> src/sat/surveyDecimationImpl.go

--- main.go
+++ main.go
@@ -1,11 +1,11 @@
 package main
 
-import sat "sid-sat"
+import "sat"
 
 import "math/rand"
 
 func main() {
 	re := rand.New(rand.NewSource(1234154342))
-	ins := sat.Random3SAT(re, 1000, 3.5)
+	ins := sat.Random3SAT(re, 2000, 4.0)
 	ins.Solve()
 }
--- src/sat/surveyDecimationImpl.go
+++ src/sat/surveyDecimationImpl.go
@@ -8,11 +8,11 @@ var tolerance float64 = 0.001
 var smooth float64 = 1.0
 
 func (ins *instance) predict() (variable, bool) {
-	numIterations := 1 + int(math.Log2(float64(len(ins.allVariables()))))
+	numIterations := 1 + int(100*math.Log2(float64(len(ins.allVariables()))))
 	var etaChange float64 = 1
 	g := ins.makePropagationGraph()
 	iterations := 0
-	for etaChange > tolerance && iterations < 1000*numIterations {
+	for etaChange > tolerance && iterations < numIterations {
 		iterations++
 		etaChange, g = ins.iteratePropagationGraph(g, smooth)
 	}
@@ -23,6 +23,7 @@ func (ins *instance) predict() (variable, bool) {
 	if trivialCover {
 		panic("trivial cover")
 	}
+	fmt.Printf("iteration: %v/%v \t", iterations, numIterations)
 	return variable, value
 }
 
@@ -34,6 +35,9 @@ func (ins *instance) Solve() map[variable]bool {
 		solution[variable] = value
 		fmt.Println(len(solution))
 		ins.reduce(variable, value)
+		if ins.emptyClause() {
+			panic("empty clause")
+		}
 	}
 	return solution
 }
