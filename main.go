733c70df803f2a00e4403d43b58974a74384bddc
Nguyen Ngoc Khanh <nguyenngockhanh.pbc@gmail.com>
2020-01-06 21:30:32 +0800

    modified:   main.go 	modified:   src/github.com/khanhhhh/sat/SidPredictImpl.go

--- main.go
+++ main.go
@@ -2,26 +2,40 @@ package main
 
 import (
 	"fmt"
-
 	"github.com/khanhhhh/sat"
 )
 
 func main() {
 	satCount := 0
-	convergedCount := 0
-	nonTrivialCount := 0
-	trueCount := 0
+	convergedCountSID := 0
+	nonTrivialCountSID := 0
+	trueCountSID := 0
+	trueCountMaxMin := 0
 	for iter := 1; ; iter++ {
-		ins := sat.Random3SAT(128, 4.0)
+		ins := sat.Random3SAT(128, 4.2)
 		isSat, _ := ins.CdclSolve()
 		if isSat {
 			satCount++
-			ok, nonTrivialCover, variable, value := ins.SidPredict()
-			if ok {
-				convergedCount++
+			{
+				ok, nonTrivialCover, variable, value := ins.SidPredict()
+				if ok {
+					convergedCountSID++
+				}
+				if ok && nonTrivialCover {
+					nonTrivialCountSID++
+					// test
+					ins.PushClause(sat.Literal{
+						Index: variable,
+						Sign:  value,
+					})
+					isSat, _ := ins.CdclSolve()
+					if isSat {
+						trueCountSID++
+					}
+				}
 			}
-			if ok && nonTrivialCover {
-				nonTrivialCount++
+			{
+				variable, value := ins.MaxMinPredict()
 				// test
 				ins.PushClause(sat.Literal{
 					Index: variable,
@@ -29,10 +43,19 @@ func main() {
 				})
 				isSat, _ := ins.CdclSolve()
 				if isSat {
-					trueCount++
+					trueCountMaxMin++
 				}
 			}
+			fmt.Printf("Precision SID   :\t%.4f\n",
+				float32(trueCountSID)/float32(nonTrivialCountSID),
+			)
+			fmt.Printf("Recall    SID   :\t%.4f\n",
+				float32(trueCountSID)/float32(satCount),
+			)
+			fmt.Printf("Precision MaxMin:\t%.4f\n",
+				float32(trueCountMaxMin)/float32(satCount),
+			)
+			fmt.Println()
 		}
-		fmt.Println(iter, satCount, convergedCount, nonTrivialCount, trueCount)
 	}
 }
--- src/github.com/khanhhhh/sat/SidPredictImpl.go
+++ src/github.com/khanhhhh/sat/SidPredictImpl.go
@@ -1,3 +1,6 @@
+// Elitza Maneva, Elchanan Mossel, and Martin J. Wainwright. 2007. A new look at survey propagation and its generalizations. J. ACM 54, 4 (July 2007), 17–es. DOI:https://doi.org/10.1145/1255443.1255445
+
+
 package sat
 
 import (
