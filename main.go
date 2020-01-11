f2fa2a7b009facef2a3dae8726c32fa87f0acffc
Nguyen Ngoc Khanh <nguyenngockhanh.pbc@gmail.com>
2020-01-11 17:10:01 +0800

    modified:   main.go

--- main.go
+++ main.go
@@ -1,72 +1,103 @@
 package main
 
 import (
-	"flag"
 	"fmt"
-	"log"
-	"os"
-	"runtime"
-	"runtime/pprof"
 	"time"
 
 	"github.com/khanhhhh/sat/guesser/surveydecimation"
 	"github.com/khanhhhh/sat/instance"
 	"github.com/khanhhhh/sat/solver/cdcl"
 	"github.com/khanhhhh/sat/solver/surveysearch"
-	"github.com/pkg/profile"
 )
 
 func main() {
-	var cpuprofile = flag.String("cpuprofile", "./cpu.prof", "write cpu profile to `file`")
-	var memprofile = flag.String("memprofile", "./mem.prof", "write memory profile to `file`")
+	/*
+		var cpuprofile = flag.String("cpuprofile", "./cpu.prof", "write cpu profile to `file`")
+		var memprofile = flag.String("memprofile", "./mem.prof", "write memory profile to `file`")
 
-	flag.Parse()
-	if *cpuprofile != "" {
-		f, err := os.Create(*cpuprofile)
-		if err != nil {
-			log.Fatal("could not create CPU profile: ", err)
+		flag.Parse()
+		if *cpuprofile != "" {
+			f, err := os.Create(*cpuprofile)
+			if err != nil {
+				log.Fatal("could not create CPU profile: ", err)
+			}
+			defer f.Close()
+			if err := pprof.StartCPUProfile(f); err != nil {
+				log.Fatal("could not start CPU profile: ", err)
+			}
+			defer pprof.StopCPUProfile()
 		}
-		defer f.Close()
-		if err := pprof.StartCPUProfile(f); err != nil {
-			log.Fatal("could not start CPU profile: ", err)
-		}
-		defer pprof.StopCPUProfile()
-	}
 
-	// ... rest of the program ...
-	defer profile.Start(profile.TraceProfile, profile.ProfilePath(".")).Stop()
+		// ... rest of the program ...
+		defer profile.Start(profile.TraceProfile, profile.ProfilePath(".")).Stop()
+	*/
 	test()
-
-	if *memprofile != "" {
-		f, err := os.Create(*memprofile)
-		if err != nil {
-			log.Fatal("could not create memory profile: ", err)
+	/*
+		if *memprofile != "" {
+			f, err := os.Create(*memprofile)
+			if err != nil {
+				log.Fatal("could not create memory profile: ", err)
+			}
+			defer f.Close()
+			runtime.GC() // get up-to-date statistics
+			if err := pprof.WriteHeapProfile(f); err != nil {
+				log.Fatal("could not write memory profile: ", err)
+			}
 		}
-		defer f.Close()
-		runtime.GC() // get up-to-date statistics
-		if err := pprof.WriteHeapProfile(f); err != nil {
-			log.Fatal("could not write memory profile: ", err)
-		}
-	}
+	*/
 }
 
 func test() {
-	test1()
+	test2()
+}
+
+func test3() {
+	ins := instance.Random3SAT(300, 4.0)
+	completeSearch := cdcl.Solve
+	t := time.Now()
+	sat, assignment := completeSearch(ins)
+	dt := time.Since(t)
+	eval, _ := ins.Evaluate(assignment)
+	fmt.Println("cdcl search:", sat, eval, dt)
+	if sat && eval {
+		for len(ins.VariableMap()) > 0 {
+			fmt.Printf("predicting variable: %v\t", len(ins.VariableMap()))
+			t := time.Now()
+			converged, nonTrivial, variableOut, valueOut := surveydecimation.Guess(ins, 1)
+			dt := time.Since(t)
+			if converged && nonTrivial {
+				// test
+				ins.Reduce(variableOut, valueOut)
+				if sat, _ := completeSearch(ins); sat == false {
+					fmt.Println("failed: unsat", dt)
+					break
+				} else {
+					fmt.Println("succeeded", dt)
+				}
+			} else {
+				fmt.Println("failed: unconverged", dt)
+				break
+			}
+		}
+	}
 }
 
 func test2() {
-	ins := instance.Random3SAT(256, 4.0)
-	{
-		t := time.Now()
-		sat, assignment := surveysearch.Solve(ins)
-		eval, _ := ins.Evaluate(assignment)
-		fmt.Println(sat, eval, time.Since(t))
-	}
-	{
-		t := time.Now()
-		sat, assignment := cdcl.Solve(ins)
-		eval, _ := ins.Evaluate(assignment)
-		fmt.Println(sat, eval, time.Since(t))
+	for {
+		ins := instance.Random3SAT(1000, 4.0)
+		{
+			t := time.Now()
+			sat, assignment := surveysearch.Solve(ins)
+			eval, _ := ins.Evaluate(assignment)
+			fmt.Println("survey search:", sat, eval, time.Since(t))
+		}
+		{
+			t := time.Now()
+			sat, assignment := cdcl.Solve(ins)
+			eval, _ := ins.Evaluate(assignment)
+			fmt.Println("cdcl   search:", sat, eval, time.Since(t))
+		}
+		fmt.Println()
 	}
 }
 
